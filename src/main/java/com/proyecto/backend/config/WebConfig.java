f7d49094314b721a9daed7618c53f74fc321d7af
Carlos138531 <137945287+Carlos138531@users.noreply.github.com>
2024-10-26 16:43:49 -0500

    Actualización del WebConfig

--- src/main/java/com/proyecto/backend/config/WebConfig.java
+++ src/main/java/com/proyecto/backend/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // Permitir todas las rutas
-                .allowedOrigins("http://pf-dawii-19.azurewebsites.net") // Cambia esto por la URL de tu frontend
+                .allowedOrigins("http://pf-dawii-19.azurewebsites.net/**") // Cambia esto por la URL de tu frontend
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Métodos permitidos
                 .allowedHeaders("*"); // Permitir todos los encabezados
     }
